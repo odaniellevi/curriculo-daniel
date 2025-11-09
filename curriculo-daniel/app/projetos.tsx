cb933c30ac7f25f6527a1e1a9b4b660c06f16252
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-11-09 03:58:07 +0000

    projetos finalizado

--- curriculo-daniel/app/projetos.tsx
+++ curriculo-daniel/app/projetos.tsx
@@ -1,7 +1,7 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 const Projetos = () => {
     return (
@@ -11,16 +11,34 @@ const Projetos = () => {
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         >
+          <ScrollView>
             <Text style={styles.title}>Projetos</Text>
             <View style={styles.section}>
                 <Image style={styles.image} source={require("../assets/images/healnetLogo.jpg")} />
                 <Text style={styles.sectionTitle}>HealNet</Text>
+                <Text style={styles.sectionText}>Landing Page de um site de marcação de consulta médica </Text>
             </View>
 
             <View style={styles.section}>
                 <Image style={styles.image} source={require("../assets/images/jogodasenha.png")} />
                 <Text style={styles.sectionTitle}>Bulls & Cows</Text>
+                <Text style={styles.sectionText}>Jogo para descobrir qual a senha gerada aleatoriamente pelo algoritmo</Text>
             </View>
+
+            <View style={styles.section}>
+              <Image style={styles.image} source={require("../assets/images/conversor.jpg")} />
+              <Text style={styles.sectionTitle}>Conversor de Valores</Text>
+              <Text style={styles.sectionText}>Site de conversões de valores e medidas.
+                 Nele estão presentes conversão de moeda, temperatura, e comprimento.</Text>
+            </View>
+
+            <View style={styles.section}>
+              <Image style={styles.image} source={require("../assets/images/apicurriculo.jpg")} />
+              <Text style={styles.sectionTitle}>API de Currículo</Text>
+              <Text style={styles.sectionText}>Projeto de back-end com informações necessárias para um currículo.</Text>
+            </View>
+            
+            </ScrollView>
         </LinearGradient>
     )
 }
@@ -57,6 +75,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff',
     marginBottom: 5,
+  },
+  sectionText: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#fff',
   },
 });
 
