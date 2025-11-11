887dfbddc78eae5e28784a76b320d485b071537e
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-11-11 05:14:10 +0000

    correção do _layout.tsx

--- curriculo-daniel/app/_layout.tsx
+++ curriculo-daniel/app/_layout.tsx
@@ -5,18 +5,33 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen 
+          name="index" 
+          options={{ title: 'Início' }}
+        />
+        <Stack.Screen 
+          name="academico" 
+          options={{ title: 'Acadêmico' }} 
+        />
+        <Stack.Screen 
+          name="experiencia" 
+          options={{ title: 'Experiência' }} 
+        />
+        <Stack.Screen 
+          name="projetos" 
+          options={{ title: 'Projetos' }} 
+        />
+        <Stack.Screen 
+          name="contato" 
+          options={{ title: 'Contato' }} 
+        />
+        
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
